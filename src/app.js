659ed2766dbae8d47212b367b37151491cf956b2
Bryan Guillen <bryan.guillen.lds@gmail.com>
2026-04-30 18:59:26 -0400

    split listen into server to allow test to run server independently without conflicting with the actual server running.

--- src/app.js
+++ src/app.js
@@ -40,10 +40,4 @@ app.use((err, req, res, next) => {
     res.status(500).json({ status: 'ERROR', message: 'Internal Server Error' });
 });
 
-// start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
 export default app;
